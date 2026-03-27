67ce8c7ceed5aa4b75ce898741367657adb215e0
freddyo21 <freddy.preo21@gmail.com>
2026-03-27 10:50:08 +0700

    build(deps): Upgrade dependencies in package.json, and refactor tsconfig.json

--- attack-visualization-system/vite.config.ts
+++ attack-visualization-system/vite.config.ts
@@ -1,7 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react()
+  ],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            if (id.includes("react") || id.includes("react-dom") || id.includes("react-router")) {
+              return "vendor-core";
+            }
+            return id.toString().split("node_modules/")[1].split("/")[0].toString();
+          }
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1000,
+  }
 })
