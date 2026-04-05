25bd701f8f18802220047106617f015b4066d030
hoangngocanh06 <ngocanh26042006@gmail.com>
2026-04-05 09:07:48 +0700

    refactor(auth): update router to use named login function

--- attack-visualization-system-be/src/routes/auth.router.ts
+++ attack-visualization-system-be/src/routes/auth.router.ts
@@ -1,8 +1,8 @@
 import { Router } from "express";
-import { AuthController } from "../controllers/auth.controller";
+import { login } from "../controllers/auth.controller";
 
 const router = Router();
 
-router.post("/login", AuthController.login);
+router.post("/login", login);
 
 export { router as authRouter };
