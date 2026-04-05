ea7c1b1d32c72ea28c4698d22ec520bfff9b0b35
hoangngocanh06 <ngocanh26042006@gmail.com>
2026-04-05 09:05:53 +0700

    refactor(auth): update service syntax to use explicit function

--- attack-visualization-system-be/src/services/auth.service.ts
+++ attack-visualization-system-be/src/services/auth.service.ts
@@ -1,42 +1,40 @@
 import { Response } from "express";
-import { AuthRepository } from "../repositories/auth.repository";
+import * as authRepository from "../repositories/auth.repository";
 import { comparePassword } from "../utils/hash";
-import { generateToken } from "../utils/jwt";
+import { generateToken } from "../utils/jwt-handler";
 import { InvalidCredentialException } from "../exceptions/InvalidCredentialException";
 
-export const AuthService = {
-  async login(emailInput: string, passwordInput: string, res: Response) {
-    const user = await AuthRepository.findByEmail(emailInput);
+export async function login(emailInput: string, passwordInput: string, res: Response) {
+  const user = await authRepository.findByEmail(emailInput);
 
-    if (!user) {
-      throw new InvalidCredentialException("Invalid email or password");
+  if (!user) {
+    throw new InvalidCredentialException("Invalid email or password");
+  }
+
+  const isMatch = await comparePassword(passwordInput, user.password);
+  if (!isMatch) {
+    throw new InvalidCredentialException("Invalid email or password");
+  }
+
+  const accessToken = generateToken(user as any);
+
+  const isProduction = process.env.NODE_ENV === "production";
+
+  res.cookie("token", accessToken, {
+    httpOnly: true,
+    secure: isProduction,
+    sameSite: "lax",
+    maxAge: 3600000
+  });
+
+  return {
+    accessToken,
+    tokenType: "Bearer",
+    expiresIn: 3600,
+    user: {
+      id: user.id,
+      email: user.email,
+      role: user.role,
     }
-
-    const isMatch = await comparePassword(passwordInput, user.password);
-    if (!isMatch) {
-      throw new InvalidCredentialException("Invalid email or password");
-    }
-
-    const accessToken = generateToken(user as any);
-
-    const isProduction = process.env.NODE_ENV === "production";
-    
-    res.cookie("token", accessToken, {
-        httpOnly: true,
-        secure: isProduction,
-        sameSite: "lax",
-        maxAge: 3600000
-    });
-
-    return {
-      accessToken,
-      tokenType: "Bearer",
-      expiresIn: 3600,
-      user: {
-        id: user.id,
-        email: user.email,
-        role: user.role,
-      }
-    };
-  },
-};
+  };
+}
