039db7b8111690e6cbbae87093c611509255322f
freddyo21 <freddy.preo21@gmail.com>
2026-04-05 17:35:13 +0700

    feat(core): implement base Exception class and IErrorResponse interface

--- attack-visualization-system-be/src/exceptions/Exception.ts
+++ attack-visualization-system-be/src/exceptions/Exception.ts
@@ -8,12 +8,13 @@
  */
 
 export class Exception extends Error {
-    public statusCode: number;
-    public details: any;
-    public isOperational: boolean;
+    protected readonly statusCode: number;
+    protected details: any;
+    protected readonly isOperational: boolean;
 
     constructor(message: string, statusCode = 500, details: any = null) {
         super(message);
+        this.name = "Exception";
         this.statusCode = statusCode;
         this.details = details;
         this.isOperational = true; // Distinguishes from programming errors
