68c5c85d723991fb6c9c0d9ed316f97fab38b84a
hoangngocanh06 <ngocanh26042006@gmail.com>
2026-04-05 09:00:44 +0700

    refactor(auth): update login controller to use explicit function

--- attack-visualization-system-be/src/controllers/auth.controller.ts
+++ attack-visualization-system-be/src/controllers/auth.controller.ts
@@ -1,23 +1,20 @@
 import { Request, Response, NextFunction } from "express";
 import { validateLoginRequest } from "../dtos/request/LoginRequestDTO";
-import { formatTokenResponse } from "../dtos/response/TokenResponseDTO";
-import { AuthService } from "../services/auth.service";
+import * as authService from "../services/auth.service";
 
-export const AuthController = {
-  async login(req: Request, res: Response, next: NextFunction) {
-    try {
-      const cleanData = validateLoginRequest(req.body);
-      
-      const rawResult = await AuthService.login(cleanData.email, cleanData.password, res);
-      
-      const safeResponse = formatTokenResponse(rawResult);
+export async function login(req: Request, res: Response, next: NextFunction) {
+  try {
+    const cleanData = validateLoginRequest(req.body);
 
-      return res.status(200).json({
-        status: "success",
-        data: safeResponse
-      });
-    } catch (err) {
-      next(err);
-    }
-  },
-};
+    const rawResult = await authService.login(cleanData.email, cleanData.password, res);
+
+    return res.status(200).json({
+      status: "success",
+      data: {
+        user: rawResult.user
+      }
+    });
+  } catch (err) {
+    next(err);
+  }
+}
